1839ef77ce2916febe67adae2759441ba1c837f9
John Doe <johndoe@example.com>
2016-06-01 22:14:58 -0700

    hey

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -4,6 +4,16 @@ class Main extends CI_Controller {
 
 	public function __construct()
 	{
+		$config['upload_path'] = './uploads/';
+		$config['allowed_types'] = 'gif|jpg|png';
+		$config['max_size']	= '1034';
+		$config['max_width'] = '1024';
+		$config['max_height'] = '768';
+
+		$this->load->library('upload', $config);
+
+		// Alternately you can set preferences by calling the initialize function. Useful if you auto-load the class:
+		$this->upload->initialize($config);
 		parent::__construct();
 		$this->load->model("Info");	}
 
