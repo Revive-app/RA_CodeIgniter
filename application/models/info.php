411d1516c3113a51808bf9a7a1dfb28504b5984e
John Doe <johndoe@example.com>
2016-06-01 18:32:15 -0700

    add where

--- application/models/info.php
+++ application/models/info.php
@@ -80,7 +80,7 @@ public function addTrainerDistance($distance){
 }
 
 public function addTrainerAbout($about){
-	$query = "INSERT INTO reviveapp.trainers (gender, trainer_id, aboutme) VALUES (?,?,?)";
+	$query = "INSERT INTO reviveapp.trainers (gender, aboutme) VALUES (?,?) WHERE trainer_id = ?";
 	$value = $about;
  $this->db->query($query, $value);
 }
