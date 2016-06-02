a6e8ce4a7ad6935ee6cb23501e6eacd4a3d7eb96
John Doe <johndoe@example.com>
2016-06-01 18:59:39 -0700

    hhe

--- application/models/info.php
+++ application/models/info.php
@@ -80,7 +80,7 @@ public function addTrainerDistance($distance){
 }
 
 public function addTrainerAbout($about){
-	$query = "UPDATE reviveapp.trainers SET gender = ? , aboutme = ? WHERE trainer_id=?";
+	$query = "UPDATE reviveapp.trainers SET gender = $about["gender"] , aboutme = $about["aboutme"] WHERE trainer_id=$about['trainer_id']";
 	$value = $about;
  $this->db->query($query, $value);
 }
